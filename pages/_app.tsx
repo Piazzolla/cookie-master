0de1c9daf06078e63146a677a3ae6722dc747166
Mariano <macacozzz@hotmail.com>
2022-10-14 17:31:17 -0300

    agrego cssbaseline para que me tome el tema

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 import { darkTheme } from '../themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   )
