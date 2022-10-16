913f12c47aa31002f31ccce076b3417730100704
Mariano <macacozzz@hotmail.com>
2022-10-16 20:15:54 -0300

    pagina de cambiar temas

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,13 +1,28 @@
+import { ChangeEvent, FC, useState } from "react";
+import { GetServerSideProps } from 'next'
+import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, Button } from '@mui/material'
+import Cookies from 'js-cookie';
 import { Layout } from "../components/layouts";
-import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
-import { ChangeEvent, useState } from "react";
+import { LineAxisOutlined } from "@mui/icons-material";
+import axios from 'axios'
 
-export const ThemeChangerPage = () => {
+interface Props {
+  theme: string;
+}
 
+export const ThemeChangerPage:FC<Props> = (props) => {
+  console.log(props);
   const [currentTheme, setCurrentTheme] = useState('light')
   const onThemeChange = ( event: ChangeEvent<HTMLInputElement>) => {
     const selectedTheme = event.target.value;
     setCurrentTheme(selectedTheme);
+
+    Cookies.set('theme', selectedTheme );
+  }
+
+  const onClick = async() =>{
+    const { data } = await axios.get('/api/hello')
+    console.log({data})
   }
 
   return (
@@ -25,10 +40,29 @@ export const ThemeChangerPage = () => {
               <FormControlLabel value='custom' control={<Radio />} label="Custom" />
             </RadioGroup>
           </FormControl>
+          <Button
+            onClick={ onClick }
+          >
+            Solicitud
+          </Button>
         </CardContent>
       </Card>
     </Layout>
   )
+}
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { theme ='light', name ='No name'} = req.cookies;
+
+  return {
+    props: {
+      theme,
+      name
+    }
+  }
 }
 
 export default ThemeChangerPage;
